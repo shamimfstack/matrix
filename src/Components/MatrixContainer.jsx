b7d10cf81f3351552e9b682766326613cf1eae88
Shamim Ahammad <shamimfstack@gmail.com>
2024-07-16 22:00:32 -0700

    functionality added

--- src/Components/MatrixContainer.jsx
+++ src/Components/MatrixContainer.jsx
@@ -1,37 +1,61 @@
 import { useState } from "react";
-import Matrix from "./Matrix";
-// import Matrix from "./Matrix";
 
 const MatrixContainer = () => {
-    const [ colors, setColors ] = useState([
-        'white', 'white', 'white', 
-        'white', 'white', 'white', 
-        'white', 'white', 'white', 
-    ]);
+  const [colors, setColors] = useState([
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+  ]);
 
-    console.log(colors.length);
+  console.log(colors.length);
 
-    const handleClick = (index) => {
-        // copying colors array immutably
-        const newColors = [...colors];
+  const handleClick = (index) => {
+    // copying colors array immutably
+    const newColors = [...colors];
 
-        // setting new colors for specific index
-        newColors[index] = 'green';
-        // updating the colors array
-        setColors(newColors);
+    // setting new colors for specific index
+    newColors[index] = "green";
+    // updating the colors array
+    setColors(newColors);
+
+    // checking all matrix are green or not
+    if (newColors.every((color) => color === "green")) {
+      // selecting matrix color to orange
+      const updatedColors = [
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+        "orange",
+      ];
+
+      // changing all matrix color to orange
+      setColors(updatedColors);
     }
-    
-    return (
-        <div className="w-[300px] grid grid-cols-3 gap-1">
-            {colors.map((color, index) => (
-                <div key={index} onClick={() => handleClick(index)} style={{backgroundColor: color}} className="w-24 h-24">
-            
-                </div>
-              
-      
-            ))}
-        </div>
-    );
-}
+  };
+
+  return (
+    <div className="w-[300px] grid grid-cols-3 gap-1">
+      {colors.map((color, index) => (
+        <div
+          key={index}
+          onClick={() => handleClick(index)}
+          style={{ backgroundColor: color }}
+          className="w-24 h-24"
+        ></div>
+      ))}
+    </div>
+  );
+};
 
 export default MatrixContainer;
